8fc89ad113ce839c5602956eecc31100aea5db85
proxmitysensor <164543841+proxmitysensor@users.noreply.github.com>
2024-04-14 00:53:44 -0400

    added support for Auth0 auth implemetation. Currently supports checks for Permissions and Scopes from Token for authorization. ApiController is for testing purposes.

---
+++ Project.Management.APIs/Auth/HasPermissionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Project.Management.APIs.Auth
+{
+    public class HasPermissionHandler : AuthorizationHandler<HasPermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasPermissionRequirement requirement)
+        {
+            // If user does not have the permission claim, get out of here
+            if (!context.User.HasClaim(c => c.Type == "permissions" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+
+            // Split the permissions string into an array
+            var permissions = context.User.FindFirst(c => c.Type == "permissions" && c.Issuer == requirement.Issuer).Value.Split(' ');
+
+            // Succeed if the permission array contains the required permission
+            if (permissions.Any(s => s == requirement.Permission))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
+
